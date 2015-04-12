a1f91182f009595a6781eeb6bfbfd9fb7f9288bc
Sergey Teplyakov <teplyakov.serg@gmail.com>
2015-04-13 01:03:25 +0300

    browser change

--- Cucumber/features/step_definitions/steps.js
+++ Cucumber/features/step_definitions/steps.js
@@ -2,7 +2,7 @@ var pc = require('protractor-cucumber');
 
 var steps = function() {
   var seleniumAddress = 'http://localhost:4444/wd/hub';
-  var options = { browser : 'firefox', timeout : 100000 };
+  var options = { browser : 'chrome', timeout : 100000 };
   this.World = pc.world(seleniumAddress, options);
 
   this.After(function(callback) {

a276fe4077c659cdb729e7229bc46d440108e250
Sergey Teplyakov <steplyakov@newmarketinc.com>
2015-04-13 13:27:00 +0300

    Commit 11

--- conf.js
+++ conf.js
@@ -1,11 +1,12 @@
 exports.config = {
 
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   specs: [
     'features/*.feature'
   ],
 
   capabilities: {
-    'browserName': 'phantomjs'
+    'browserName': 'phantomjs',
   },
 
   framework: 'cucumber'
